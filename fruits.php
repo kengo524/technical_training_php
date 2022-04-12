d77c60b62884242fc2cc6dc0874ff264f2c6b2bd
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-04-12 15:37:36 +0900

    リファクタリング（ファイル名変更＋指摘事項修正）

--- fruits.php
+++ fruits.php
@@ -1,52 +1,49 @@
 <?php
     class Fruits{/*クラスを定義する。*/
 
-        private $name = "";
+        private $names = "";
         private $max_price = "";
         private $min_price = "";
         private $quantity = "";
-        private $data = [];
-        /*初期化 */
-        function __construct($name,$max_price,$min_price,$quantity){
-            $this->name = $name;
+        private $prices = [];
+
+        function __construct($names,$max_price,$min_price,$quantity){/*初期化 */
+            $this->names = $names;
             $this->max_price = $max_price;
             $this->min_price = $min_price;
             $this->quantity = $quantity;
             for ($i = 0; $i < $this->quantity; $i++){
-                $this->data[] =  rand($this->min_price,$this->max_price);
+                $this->prices[] =  rand($this->min_price,$this->max_price);
             }
         }
 
         function getMinPrice(){   /*要素から最小値を求める関数 */ 
-            return(min($this->data));
+            return(min($this->prices));
         }
 
         function getMaxPrice(){/*要素から最大値を求める関数 */
-            return(max($this->data));
+            return(max($this->prices));
         }
 
         function getAveragePrice(){/*要素から平均値を求める関数（小数点以下、四捨五入）。 */
-            return(round(array_sum($this->data)/$this->quantity));
+            return(round(array_sum($this->prices)/$this->quantity));
         }
 
-        function getPrice(){
-            echo "{$this->name}の価格\n";
+        function printStatistics(){
+            echo "{$this->names}の価格";
+            echo "<br />";
             echo "最高値は、";
             print_r($this->getMaxPrice());
-            echo "円です。\n"; 
+            echo "円です。";
+            echo "<br />"; 
             echo "最安値は、";
             print_r($this->getMinPrice()); 
-            echo "円です。\n"; 
+            echo "円です。";
+            echo "<br />"; 
             echo "平均価格は、";
             print_r($this->getAveragePrice());
-            echo "円です。（小数点以下、四捨五入）\n"; 
+            echo "円です。（小数点以下、四捨五入）";
+            echo "<br />"; 
         }
     }
-
-    $peach = new Fruits("桃",300,200,15);
-    $peach->getPrice();
-    
-    
-    $strawberry = new Fruits("イチゴ",400,500,15);
-    $strawberry->getPrice();
 ?>
